43b17a7ba58ee897d63a1b56ee80916d4ae370e8
junqi1 <junqi1@staff.weibo.com>
2018-05-06 17:34:19 +0800

    文章模块

--- miniProgramContest/pages/articlePage/articlePage.js
+++ miniProgramContest/pages/articlePage/articlePage.js
@@ -1,86 +1,111 @@
-
-// pages/articlePage/articlePage.js
+import { Articlepage } from './articlePage-model.js'
+var articlepage = new Articlepage()
 
 
 const $swipertab = [
-  { name: '精选', index: 0, isCurr: true,  },
-  { name: '宝宝', index: 1, isCurr: false, },
-  { name: '瘦身', index: 2, isCurr: false, },
-  { name: '恋爱', index: 3, isCurr: false, },];
-  const articleArr = [
-    {imgUrl:'/icons/article.png',title:'1岁宝宝不能吃什么？',flag:'1',readNum:'800'},
+    { name: '精选', index: 0, isCurr: true, },
+    { name: '宝宝', index: 1, isCurr: false, },
+    { name: '瘦身', index: 2, isCurr: false, },
+    { name: '恋爱', index: 3, isCurr: false, },];
+const articleArr = [
     { imgUrl: '/icons/article.png', title: '1岁宝宝不能吃什么？', flag: '1', readNum: '800' },
     { imgUrl: '/icons/article.png', title: '1岁宝宝不能吃什么？', flag: '1', readNum: '800' },
     { imgUrl: '/icons/article.png', title: '1岁宝宝不能吃什么？', flag: '1', readNum: '800' },
     { imgUrl: '/icons/article.png', title: '1岁宝宝不能吃什么？', flag: '1', readNum: '800' },
-  ]
+    { imgUrl: '/icons/article.png', title: '1岁宝宝不能吃什么？', flag: '1', readNum: '800' },
+]
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    swipertab: $swipertab,
-    articleArr: articleArr,
-    currentTab: 0, //默认从第一个开始
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        // swipertab: $swipertab,
+        // articleArr: articleArr,
+        currentTab: 0, //默认从第一个开始
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        var that = this;
+        articlepage.getCategoryList((res) => {
+            that.setData({
+                swipertab: res
+            });
+        });
+
+        let id = 1;
+        articlepage.getArticleList((res) => {
+            that.setData({
+                articleArr: res
+            });
+        }, id);
+    },
 
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-  
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
 
-  /**
-   * 顶部TAB切换
-   */
-  tabClick: function(e) {
-    let current = e.currentTarget.dataset.current
-    if (this.data.currentTab == current) {
-      return false;
-    } else {
-      this.setData({
-        currentTab: current
-      })
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    },
+
+    /**
+     * 顶部TAB切换
+     */
+    tabClick: function (e) {
+        let current = e.currentTarget.dataset.current
+        if (this.data.currentTab == current) {
+            return false;
+        } else {
+            this.setData({
+                currentTab: current
+            })
+        }
+    },
+
+    onPostTap: function(e){
+        var that = this;
+        let id = e.currentTarget.dataset.id;
+        articlepage.getArticleList((res) => {
+            that.setData({
+                articleArr: res
+            });
+        }, id);
+    },
+
+    gotoDetail: function (e) {
+        var content = e.currentTarget.dataset.content;
+        var tag = e.currentTarget.dataset.tag;
+        var read = e.currentTarget.dataset.read;
+        var title = e.currentTarget.dataset.title;
+        var pic = e.currentTarget.dataset.pic;
+        wx.navigateTo({
+            url: './articleDetail/articleDetail?content='+content+"&tag="+tag+"&read="+read+"&title="+title+"&pic="+pic
+        })
     }
-  },
-
-  gotoDetail:function(e){
-
-    let id = e.currentTarget.id
-    wx.navigateTo({
-      url: './articleDetail/articleDetail',
-    })
-  }
 })
